03a6f33d1ffa68c748ecf9ced01c7715402164cc
landonb <landonb@users.noreply.github.com>
2020-03-15 15:43:36 -0500

    Docs: Update headers.

--- bin/completions/git-completion.bash
+++ bin/completions/git-completion.bash
@@ -1,3 +1,11 @@
+#!/bin/bash
+# vim:tw=0:ts=4:sw=4:noet:norl:ft=sh
+# Author: https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+# Project: https://github.com/landonb/git-FlU#🌡
+# License: GPLv2 [this file only; bulk of git-FlU is MIT per LICENSE file].
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
 # bash/zsh completion support for core Git.
 #
 # Copyright (C) 2006,2007 Shawn O. Pearce <spearce@spearce.org>
